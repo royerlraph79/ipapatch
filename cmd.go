848402763092af0611856bc7a301193133d824a4
royerlraph79 <30907279+royerlraph79@users.noreply.github.com>
2025-11-15 02:10:35 -0500

    cmd.go

--- cmd.go
+++ cmd.go
@@ -7,12 +7,13 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-const helpText = `usage: ipapatch [-h/--help] [-i/--input <path>] [-o/--output <path>] [-d/--dylib <path>] [-f/--inplace] [-y/--noconfirm/] [-p/--plugins-only] [-z/--zip] [--version]
+const helpText = `usage: ipapatch [-h/--help] [-i/--input <path>] [-o/--output <path>] [-d/--dylib <path>] [-f/--inplace] [-y/--noconfirm] [-p/--plugins-only] [-z/--zip] [--version]
 
 flags:
   -i, --input path      the path to the ipa file to patch (required)
   -o, --output path     the path to the patched ipa file to create
-  -d, --dylib path      the path to the dylib to use instead of the embedded zxPluginsInject
+  -d, --dylib path      the path(s) to dylib(s) to use instead of the embedded zxPluginsInject
+                        repeat multiple times: -d tweak1.dylib -d tweak2.dylib
   -f, --inplace         takes priority over --output, use this to overwrite the input file
   -y, --noconfirm       skip interactive confirmation when not using --inplace
   -p, --plugins-only    only inject into plugin binaries (not the main executable)
@@ -23,13 +24,13 @@ info:
   --version             show version and exit`
 
 type Args struct {
-	Input       string `arg:"-i,--input,required"`
-	Output      string `arg:"-o,--output"`
-	Dylib       string `arg:"-d,--dylib"`
-	InPlace     bool   `arg:"-f,--inplace"`
-	NoConfirm   bool   `arg:"-y,--noconfirm"`
-	PluginsOnly bool   `arg:"-p,--plugins-only"`
-	UseZip      bool   `arg:"-z,--zip"`
+	Input       string   `arg:"-i,--input,required"`
+	Output      string   `arg:"-o,--output"`
+	Dylib       []string `arg:"-d,--dylib,separate"`
+	InPlace     bool     `arg:"-f,--inplace"`
+	NoConfirm   bool     `arg:"-y,--noconfirm"`
+	PluginsOnly bool     `arg:"-p,--plugins-only"`
+	UseZip      bool     `arg:"-z,--zip"`
 }
 
 func (Args) Version() string {
